dec0302bb0467b54590fa0b26c7da4e3c50b5c5a
MBaetti <maurice.baettig@stud.hslu.ch>
2026-05-06 20:46:20 +0200

    feat(metaprogramming): finished up to unquote

--- lib/metaprogramming.exs
+++ lib/metaprogramming.exs
@@ -1,5 +1,64 @@
 # Metaprogrammierung in Elixir
 # Skript ausführen: elixir metaprogramming.exs
 # Interaktive Shell starten: iex.bat -S mix
+# File in die Shell laden: import_file("./lib/metaprogramming.exs")
 
-IO.puts("Hallo aus dem Elixir-Skript!")
+# - Quote -
+# quote gibt abstract syntax tree zurück, also die Struktur des Codes, ohne ihn auszuführen
+# Das zurückgegebene Tuple besteht aus:
+# {
+#  Operator,
+#  Metadaten,
+#  Funktionsargumente
+# }
+
+quote do: 1 + 2
+# {
+#  :+,
+#  [context: Elixir, imports: [{1, Kernel}, {2, Kernel}]],
+#  [1, 2]
+# }
+
+quote do: if(value, do: "True", else: "False")
+# {
+#  :if,
+#  [context: Elixir, imports: [{2, Kernel}]],
+#  [{:value, [], Elixir}, [do: "True", else: "False"]]
+# }
+
+# - Unquote -
+# unquote ermöglicht es, Werte in einen quote-Ausdruck einzufügen, damit sie zur Kompilierzeit ausgewertet werden können
+
+value = 42
+
+quote do: 1 + value
+# {
+#  :+,
+#  [context: Elixir, imports: [{1, Kernel}, {2, Kernel}]],
+#  [1, {:value, [], Elixir}]
+# }
+
+quote do: 1 + unquote(value)
+# {
+#  :+,
+#  [context: Elixir, imports: [{1, Kernel}, {2, Kernel}]],
+#  [1, 42]
+# }
+# 42 wird in den quote-Ausdruck eingefügt und zur Kompilierzeit ausgewertet
+
+# -- Makros --
+defmodule OurMacro do
+  defmacro unless(expr, do: block) do
+    quote do
+      if !unquote(expr), do: unquote(block)
+    end
+  end
+end
+
+require OurMacro
+
+OurMacro.unless(true, do: "Hi")
+OurMacro.unless(false, do: "Hi")
+
+# - Ende -
+IO.puts("Metaprogramming erfolgreich geladen.")
