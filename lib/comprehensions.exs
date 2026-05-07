aecbcc96fe19df20e3adb2ddf7b3105e2a6ac55a
Mathias Vogel <mathias.vo@outlook.com>
2026-05-07 15:49:32 +0200

    feat(comprehensions): add examples

--- lib/comprehensions.exs
+++ lib/comprehensions.exs
@@ -2,4 +2,22 @@
 # Skript ausführen: elixir comprehensions.exs
 # Interaktive Shell starten: iex.bat -S mix
 
-quote do: 1 + 2
+# Liste mit Namen
+names = ["Joe", "Tara", "Sue", "Tim"]
+
+# Mit Comprehensions
+listResult = for name <- names,
+             String.starts_with?(name, "T"),
+             do: String.upcase(name)
+
+stringResult = Enum.join(listResult, " ")
+IO.puts(stringResult)
+
+
+# Mit dem Pipe-Operator (& definiert eine anonyme Funktion)
+stringResult2 = names
+                |> Enum.filter(&String.starts_with?(&1, "T"))
+                |> Enum.map(&String.upcase/1)
+                |> Enum.join(" ")
+
+IO.puts(stringResult2)
---
+++ lib/interesting-features.exs
@@ -0,0 +1,5 @@
+# Typen vergleichen
+# number < atom < reference < function < port < pid < tuple < map < list < bitstring
+
+result = 40 < "30"
+IO.puts(result)
