9912e4d2bb30552c64ec4a3ce8561a752788452e
Mathias Vogel <mathias.vo@outlook.com>
2026-05-25 23:36:53 +0200

    feat: update code snippets

--- lib/comprehensions.exs
+++ lib/comprehensions.exs
@@ -5,6 +5,7 @@
 # Liste mit Namen
 names = ["Joe", "Tara", "Sue", "Tim"]
 
+
 # Mit Comprehensions
 listResult = for name <- names,
              String.starts_with?(name, "T"),
@@ -21,3 +22,21 @@ stringResult2 = names
                 |> Enum.join(" ")
 
 IO.puts(stringResult2)
+
+
+# Mit Comprehensions und dem into-Option
+stringResult3 = for name <- names,
+             String.starts_with?(name, "T"),
+             into: "",
+             do: String.upcase(name) <> " "
+
+IO.puts(String.trim(stringResult3))
+
+
+# Mit Comprehensions und dem into-Option, um ein MapSet zu erstellen
+numbers = [1, 2, 2, 3, 3]
+
+numberResult = for x <- numbers, into: MapSet.new(), do: x
+for x <- numberResult do
+  IO.puts(x)
+end
--- lib/concurrency.exs
+++ lib/concurrency.exs
@@ -2,4 +2,42 @@
 # Skript ausführen: elixir concurrency.exs
 # Interaktive Shell starten: iex.bat -S mix
 
-IO.puts("Hallo aus dem Elixir-Skript!")
+defmodule Demo do
+  def run do
+    main = self()
+
+    # Task 3
+    pid3 = spawn(fn ->
+      receive do
+        {:step3, val} ->
+          :timer.sleep(3000)
+          IO.write(val)
+          send(main, :done)
+      end
+    end)
+
+    # Task 2 braucht pid3
+    pid2 = spawn(fn ->
+      receive do
+        {:step2, val} ->
+          :timer.sleep(3000)
+          IO.write("2")
+          send(pid3, {:step3, "The answer is #{val}"})
+      end
+    end)
+
+    # Task 1 braucht pid2
+    spawn(fn ->
+      :timer.sleep(3000)
+      IO.write("1")
+      send(pid2, {:step2, "42"})
+    end)
+
+    IO.puts("-> Now waiting for things to happen!")
+    for _ <- 1..20, do: (IO.write("."); :timer.sleep(500))
+
+    receive do: (:done -> IO.puts("\n-> Done."))
+  end
+end
+
+Demo.run()
